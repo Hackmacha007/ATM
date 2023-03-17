a34be16c8a3cc12084ae62117826a4f83cbee4e6
Raghavendra Goud <61324448+Hackmacha007@users.noreply.github.com>
2023-03-17 19:13:14 +0530

    Add files via upload

---
+++ pages/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#currentBtn").click(function() {
+		window.location.href = "current.html";
+	});
+
+	$("#savingsBtn").click(function() {
+		window.location.href = "savings.html";
+	});
+});
+
